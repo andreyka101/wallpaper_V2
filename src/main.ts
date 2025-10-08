a9b89a10403064c078c46fc3c3dd5851927bc52c
andreyka101 <andrejkvasnin236@gmail.com>
2025-10-08 19:03:10 +0300

    dwwdwd

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import './style.scss'
+const body = document.querySelector("body") as HTMLBodyElement
+
+body.style.width = window.innerWidth + "px"
+body.style.height = window.innerHeight + "px"
+console.log(window.innerWidth);
+console.log(window.innerHeight);
+
+
