dfa722d19cd631a15d723a9517499f7e46aac5c4
andreyka101 <andrejkvasnin236@gmail.com>
2025-12-08 00:59:21 +0300

    лглгллг

--- vite.config.ts
+++ vite.config.ts
@@ -7,16 +7,6 @@ export default {
       input: {
         // @ts-ignore
         main: resolve(__dirname, 'index.html'),
-        // @ts-ignore
-        movie: resolve(__dirname, '/src/movie_search/movie.html'),
-        // @ts-ignore
-        ALLpackages: resolve(__dirname, '/src/all_packages-nodeJS/packagesH.html'),
-        // @ts-ignore
-        passing_json: resolve(__dirname, '/src/passing_json/passing_json_INDEX.html'),
-        // @ts-ignore
-        example_database: resolve(__dirname, '/src/example_database/e_database_i.html'),
-        // @ts-ignore
-        vegetables_and_fruits: resolve(__dirname, "/src/vegetables_and_fruits/v-a-fruits.html"),
       }
     }
   }
