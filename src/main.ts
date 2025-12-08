4b834f11db664820259033ca6a765d3ceb0910fb
andreyka101 <andrejkvasnin236@gmail.com>
2025-12-08 20:57:01 +0300

    цввцвцв

--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,74 @@
-import './style.scss'
-const body = document.querySelector("body") as HTMLBodyElement
-const div = document.querySelector("div div") as HTMLDivElement
+// import './style.scss'
+// const body = document.querySelector("body") as HTMLBodyElement
+// const div = document.querySelector("div div") as HTMLDivElement
 
 
-// async function dislpayBatteryStatus(){
+// // async function dislpayBatteryStatus(){
+// //     const batteryManager = await navigator.getBattery();
+// //     console.log("is charging: ", batteryManager.charging);
+// //     console.log("charging time (sec): ", batteryManager.chargingTime);
+// //     console.log("discharging time (sec): ", batteryManager.dischargingTime);
+// //     console.log("charge level: ", batteryManager.level);
+// // }
+// // dislpayBatteryStatus();
+
+// body.style.width = window.innerWidth + "px"
+// body.style.height = window.innerHeight + "px"
+// console.log(window.innerWidth);
+// console.log(window.innerHeight);
+// const siteWidth = window.innerWidth
+// const siteHeight = window.innerHeight
+// const min = -1;
+// const max = 99;
+// const randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+
+
+// setInterval(async ()=>{
 //     const batteryManager = await navigator.getBattery();
-//     console.log("is charging: ", batteryManager.charging);
-//     console.log("charging time (sec): ", batteryManager.chargingTime);
-//     console.log("discharging time (sec): ", batteryManager.dischargingTime);
-//     console.log("charge level: ", batteryManager.level);
-// }
-// dislpayBatteryStatus();
-
-body.style.width = window.innerWidth + "px"
-body.style.height = window.innerHeight + "px"
-console.log(window.innerWidth);
-console.log(window.innerHeight);
-const siteWidth = window.innerWidth
-const siteHeight = window.innerHeight
-const min = -1;
-const max = 99;
-const randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+//     if(batteryManager.charging){
+//         const num = Math.floor(Math.random() * (300 - 25 + 1)) + 25
+//         div.style.top = Math.floor(Math.random() * ((num/2 + siteHeight) - num/2 + 1)) + -num + "px"
+//         div.style.left = Math.floor(Math.random() * ((num/2 + siteWidth) - num/2 + 1)) + -num + "px"
+//         div.style.width = num + "px"
+//         div.style.height = num + "px"
+//         div.style.filter = 'blur(' + Math.floor(Math.random() * (70 - 10 + 1)) + 10 + 'px)'
+//     }
+// }, 3000);
 
 
-setInterval(async ()=>{
-    const batteryManager = await navigator.getBattery();
-    if(batteryManager.charging){
-        const num = Math.floor(Math.random() * (300 - 25 + 1)) + 25
-        div.style.top = Math.floor(Math.random() * ((num/2 + siteHeight) - num/2 + 1)) + -num + "px"
-        div.style.left = Math.floor(Math.random() * ((num/2 + siteWidth) - num/2 + 1)) + -num + "px"
-        div.style.width = num + "px"
-        div.style.height = num + "px"
-        div.style.filter = 'blur(' + Math.floor(Math.random() * (70 - 10 + 1)) + 10 + 'px)'
-    }
-}, 3000);
 
+
+
+
+
+
+// ---------------------------------------------- 
+
+
+
+
+
+
+
+// body.style.width = window.innerWidth + "px"
+// body.style.height = window.innerHeight + "px"
+// // console.log(window.innerWidth);
+// // console.log(window.innerHeight);
+// const siteWidth = window.innerWidth
+// const siteHeight = window.innerHeight
+// const min = -1;
+// const max = 99;
+// const randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+
+
+// const intervalId = setInterval(function() {
+//         // const num = Math.floor(Math.random() * (300 - 25 + 1)) + 25
+//         // console.log("top" , Math.random() * (300 - 25 + 1));
+//         // div.style.top = Math.floor(Math.random() * ((num/2 + siteHeight) - num/2 + 1)) + -num + "px"
+//         // console.log("left" , Math.random() * (300 - 25 + 1));
+//         // div.style.left = Math.floor(Math.random() * ((num/2 + siteWidth) - num/2 + 1)) + -num + "px"
+//         // div.style.width = num + "px"
+//         // div.style.height = num + "px"
+//         console.log("filter" , Math.random() * (300 - 25 + 1));
+//         // div.style.filter = 'blur(' + Math.floor(Math.random() * (30 - 10 + 1)) + 10 + 'px)'
+// }, 3);
